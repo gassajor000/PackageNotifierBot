087e03b321dfa3d53277f62e04ae88466d46e868
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-09-25 16:55:17 -0700

    Finish Package notifier tests.

--- test/TestPackageNotifier.py
+++ test/TestPackageNotifier.py
@@ -3,6 +3,7 @@
     TestPackageNotifier: unit tests for package notifier class
 """
 import datetime
+import re
 import unittest
 from unittest import mock
 
@@ -51,6 +52,11 @@ class MockDB(mock.Mock):
         self._getPackage(id)
         return self.packages.get(id)
 
+    def getUncollectedPackages(self):
+        self._getUncollectedPackages()
+        packages = filter(lambda p: not p.collected, self.packages.values())
+        return list(packages)
+
     def claimPackage(self, package:Package):
         self._claimPackage(package)
         self.packages[package.id].collected = True
@@ -71,6 +77,8 @@ class MockDB(mock.Mock):
 
 
 class MockRequestResult(mock.Mock):
+    URL_REGEX = re.compile('(/)([0-9]*)(\?)')
+
     @classmethod
     def setUsers(cls, users):
         cls.users = users
@@ -78,18 +86,20 @@ class MockRequestResult(mock.Mock):
     def __init__(self, url, *args, **kwargs):
         super(MockRequestResult, self).__init__(*args, **kwargs)
         self.url = url
-        self.pfid = 'something'     # TODO actually parse the URL
+        match = self.URL_REGEX.search(url)
+        self.pfid = match.group(2) if match else None
 
     def json(self):
         user = self.users.get(self.pfid)
         if user:
-            first_name, last_name = user.name.split
+            first_name, last_name = user.name.split(' ')
             return {'first_name': first_name, 'last_name': last_name}
 
         return {'first_name': 'Unknown', 'last_name': 'Unknown'}
 
 class MockRequestLib(mock.Mock):
     def get(self, url):
+        self._get(url)
         return MockRequestResult(url)
 
 class FakeMessage(dict):
@@ -99,14 +109,25 @@ class FakeMessage(dict):
         self['sender'] = {'id': sender.PFID}
         self['message'] = {'text': msg}
 
+class FakeEmail():
+    def __init__(self, body, code):
+        self.code = code
+        self.body = body
+
+    @classmethod
+    def from_package(cls, package):
+        return FakeEmail('New package pickup code\n{}'.format(package.code), str(package.code))
+
+
 
 MOCK_DB = MockDB(name='mock db')
 MOCK_BOT = mock.Mock(name='mock bot')
 MOCK_PNBDATABASE_LIB = mock.MagicMock(return_value=MOCK_DB, name='mock db lib')
 MOCK_PYMESSENGER_LIB = mock.MagicMock(return_value=MOCK_BOT, name='mock pymessenger lib')
+MOCK_REQUESTS_LIB = MockRequestLib()
 
 modules = {'PNBDatabase': mock.MagicMock(PNBDatabase=MOCK_PNBDATABASE_LIB, User=User, Package=Package),
-           'requests': MockRequestLib(),
+           'requests': MOCK_REQUESTS_LIB,
            'pymessenger.bot': mock.MagicMock(Bot=MOCK_PYMESSENGER_LIB)}
 
 with mock.patch.dict('sys.modules', modules):
@@ -120,13 +141,6 @@ class TestPackageNotifier(unittest.TestCase):
         cls.test_user2 = User.newUser('102', 'Vanya Hargreaves')
         cls.test_user3 = User.newUser('103', 'Luther Hargreaves')
         cls.test_user4 = User.newAdmin('104', 'Allison Hargreaves')
-
-        today = datetime.date.today()
-        cls.test_package1 = Package.newPackage(1234, today)
-        cls.test_package2 = Package.newPackage(5678, today)
-        cls.test_package3 = Package.newPackage(9012, today)
-        cls.test_package4 = Package.newPackage(3456, today)
-        cls.test_package2.collected = True
 
         cls.globals = [MOCK_DB, MOCK_BOT, MOCK_PNBDATABASE_LIB, MOCK_PYMESSENGER_LIB]
 
@@ -143,8 +157,20 @@ class TestPackageNotifier(unittest.TestCase):
         # reset db
         MOCK_DB.reset()
         users = [self.test_user1, self.test_user2, self.test_user3]
+
+        today = datetime.date.today()
+        self.test_package1 = Package.newPackage(1234, today)
+        self.test_package2 = Package.newPackage(5678, today)
+        self.test_package3 = Package.newPackage(9012, today)
+        self.test_package4 = Package.newPackage(3456, today)
+        self.test_package2.collected = True
         packages = [self.test_package1, self.test_package2, self.test_package3]
+        
         MOCK_DB.load(users, packages)
+        MOCK_BOT.reset_mock()
+        MOCK_REQUESTS_LIB.reset_mock()
+        MOCK_PNBDATABASE_LIB.reset_mock()
+        MOCK_PYMESSENGER_LIB.reset_mock()
 
 
     def testInit(self):
@@ -183,10 +209,17 @@ class TestPackageNotifier(unittest.TestCase):
         """Help command returns help text. Help text for users does not include admin commands."""
         pn = PackageNotifier('test_auth_token')
 
+        user_cmds = ['list packages', 'claim package', 'unsubscribe', 'help']
+        admin_cmds = user_cmds + ['list users', 'remove user']
+
         # Query Help
         msg = FakeMessage(self.test_user1, 'help')
         pn.handle_message(msg)
         MOCK_BOT.send_text_message.assert_called_once_with(self.test_user1.PFID, pn.HELP_TEXT_ADMIN)
+
+        # check for admin commands
+        for cmd in admin_cmds:
+            self.assertIn(cmd, pn.HELP_TEXT_ADMIN, "Admin help missing {} cmd!".format(cmd))
 
         MOCK_BOT.reset_mock()
 
@@ -194,8 +227,12 @@ class TestPackageNotifier(unittest.TestCase):
         pn.handle_message(msg)
         MOCK_BOT.send_text_message.assert_called_once_with(self.test_user2.PFID, pn.HELP_TEXT)
 
+        # Make sure admin cmds aren't in the non admin msg
         self.assertNotIn('list users', pn.HELP_TEXT)
         self.assertNotIn('remove user', pn.HELP_TEXT)
+
+        for cmd in user_cmds:
+            self.assertIn(cmd, pn.HELP_TEXT, "User help missing {} cmd!".format(cmd))
 
     def testListPackagesCmd(self):
         """list packages will list all unclaimed packages"""
@@ -204,11 +241,12 @@ class TestPackageNotifier(unittest.TestCase):
         # Add a user
         msg = FakeMessage(self.test_user1, 'list packages')
         pn.handle_message(msg)
-        self.assertEqual(MOCK_BOT.send_text_message.call_count, 2)
-        msgs = MOCK_BOT.send_text_message.call_args
+        self.assertEqual(MOCK_BOT.send_text_message.call_count, 2, "Incorrect number of packages returned!")
 
-        # TODO finish this
-        self.fail("Unfinished test!")
+        pk_msgs_args = MOCK_BOT.send_text_message.call_args_list
+        for pkg in [self.test_package1, self.test_package3]:
+            # check msg was sent with package info
+            self.assertTrue(any([pk_msg_arg[0][1] == str(pkg) for pk_msg_arg in pk_msgs_args]))
 
     def testClaimPackageCmd(self):
         """claim package will mark the package as collected"""
@@ -250,12 +288,86 @@ class TestPackageNotifier(unittest.TestCase):
 
     def testListUsersCmd(self):
         """list users lists all active users. Cannot be called by non-admin."""
+        pn = PackageNotifier('test_auth_token')
+
+        # List users w/ user privileges
+        msg = FakeMessage(self.test_user2, 'list users')
+        pn.handle_message(msg)
+        self.assertEqual(MOCK_BOT.send_text_message.call_count, 1)
+        self.assertEqual(MOCK_BOT.send_text_message.call_args[0][1], pn.UNKNOWN_CMD_TEXT)
+
+        MOCK_BOT.reset_mock()
+        msg = FakeMessage(self.test_user1, 'list users')
+        pn.handle_message(msg)
+        self.assertEqual(MOCK_BOT.send_text_message.call_count, 1 , "Incorrect number of messages sent!")
+
+        users = MOCK_BOT.send_text_message.call_args[0][1].split('\n')
+        users.pop(0)    # remove header
+        self.assertEqual(len(users), 3 , "Incorrect number of users listed!")
+        for user in [self.test_user1, self.test_user2, self.test_user3]:
+            # check msg was sent with user info
+            self.assertTrue(any([user_msg == str(user) for user_msg in users]), "User {} not found!".format(user.name))
 
     def testHandleEmail(self):
         """handle_email adds the new package to the db and messages all active users."""
+        pn = PackageNotifier('test_auth_token')
+
+        # Different possible email formats
+        emails = [
+            FakeEmail.from_package(self.test_package4),
+            FakeEmail('blah blah blah pickup code\n5678\n', '5678'),
+            FakeEmail('blah blah blah Pickup Code\r\n656558 lorem ipsum', '656558'),
+            FakeEmail('blah blah blah Pickup Code    99999999', '99999999'),
+        ]
+        for email in emails:
+            MOCK_BOT.reset_mock()
+            pn.handle_email(email)
+
+            self.assertEqual(MOCK_BOT.send_text_message.call_count, 3, "Incorrect number of messages sent out!")
+            self.assertIn(email.code, MOCK_BOT.send_text_message.call_args[0][1], "Message did not contain pickup code!")
+
+        # Handle bad email no pickup code)
+        bad_email = FakeEmail('blah blah blah no code', '99999999')
+        MOCK_BOT.reset_mock()
+        pn.handle_email(bad_email)
+
+        self.assertEqual(MOCK_BOT.send_text_message.call_count, 1, "Incorrect number of messages sent out!")
+        self.assertIn("no pickup code", MOCK_BOT.send_text_message.call_args[0][1].lower(), "Message did not indicate an error")
 
     def testGetUserName(self):
         """when creating a new user, PackageNotifier correctly queries the Facebook API for the full name"""
+        pn = PackageNotifier('test_auth_token')
+
+        # Add a user
+        msg = FakeMessage(self.test_user4, 'hedwig')
+        pn.handle_message(msg)
+        MOCK_REQUESTS_LIB._get.assert_called_once_with(
+            "https://graph.facebook.com/{}?fields=first_name,last_name&access_token={}".format(self.test_user4.PFID,
+                                                                                               'test_auth_token'))
+        self.assertEqual(self.test_user4.name, MOCK_DB.users.get(self.test_user4.PFID).name)
+
+        MOCK_REQUESTS_LIB.reset_mock()
+        MOCK_DB.reset()
+
+        # add an admin
+        msg = FakeMessage(self.test_user4, 'errol')
+        pn.handle_message(msg)
+        MOCK_REQUESTS_LIB._get.assert_called_once_with(
+            "https://graph.facebook.com/{}?fields=first_name,last_name&access_token={}".format(self.test_user4.PFID,
+                                                                                               'test_auth_token'))
+        self.assertEqual(self.test_user4.name, MOCK_DB.users.get(self.test_user4.PFID).name)
 
     def testUnknownUser(self):
         """No commands work if a user is not registered"""
+        pn = PackageNotifier('test_auth_token')
+
+        cmds = ['list users', 'list packages', 'claim package 2',
+                'remove user Luther Hargreaves', 'unsubscribe', 'help']
+
+        for cmd in cmds:
+            MOCK_BOT.reset_mock()
+            msg = FakeMessage(self.test_user4, cmd)
+            pn.handle_message(msg)
+
+            MOCK_BOT.send_text_message.assert_called_once_with(self.test_user4.PFID, pn.HELP_TEXT_UNVERIFIED)
+
